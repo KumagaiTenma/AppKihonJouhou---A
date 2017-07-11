db92f97f286231b0f8a28b895c7ef47ecc0c2e7b
Your Name <tenma.kumagai.sys@gmail.com>
2017-07-11 10:42:22 +0900

    saigo

---
+++ src/AppKihonJouhou.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AppKihonJouhou {
+	int point;
+	int total;
+	static Scanner sc;
+
+	public static void main(String[] args){
+		sc = new Scanner(System.in);
+	}
+	public int question1(){
+		    return point;
+    }
+	public int question2(){
+		    return point;
+	}
+	public int question3(){
+		    return point;
+	}
+	public int question4(){
+		    return point;
+	}
+	public int question5(){
+		    return point;
+	}
+	public int question6(){
+		    return point;
+	}
+
+}
