719e7de6872fe8dbe0e065e71b68567138a9a4d9
Nakayama Ryuichi <ryuichi.nakayama.sys@gmail.com>
2017-07-11 12:01:42 +0900

    確定

--- src/AppKihonJouhou.java
+++ src/AppKihonJouhou.java
@@ -15,6 +15,26 @@ public class AppKihonJouhou {
 		    return point;
 	}
 	public int question3(){
+		int i = 0;
+
+		System.out.println("問題：ルータの機能に関する記述として，適切なものはどれか。");
+		System.out.println("１．LAN同土やLANとWANを接続して，ネットワーク層での中継処理を行う。");
+		System.out.println("２．データ伝送媒体上の信号を物理層で増幅して中継する。");
+		System.out.println("３．データリンク層でネットワーク同士を接続する。");
+		System.out.println("４．二つ以上のLANを接続し，LAN上のMACアドレスを参照して，データフレームをほかのセグメントに流すかどうかの判断を行う。");
+
+
+
+		System.out.println("解答：" + i = sc.nextInt());
+		if(i  == 1){
+			System.out.println("正解");
+			point = 1;
+		}
+		else if(i >= 2 && i < 4){
+			System.out.println("不正解");
+		}
+		else
+			System.out.println("1から4の数字を選択してください");
 		    return point;
 	}
 	public int question4(){
